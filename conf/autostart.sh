b4b2eba72c4a5430dc1a530367d11db3db10a723
m0gjr <m0gjr@github.com>
2019-08-14 19:17:19 +0100

    initial commit

---
+++ conf/autostart.sh
@@ -0,0 +1,15 @@
+mouse&
+wifistart&
+
+xsetroot -solid black
+
+setxkbmap gb
+setxkbmap -option ctrl:nocaps
+xmodmap -e "keycode 108 = Alt_L"
+xmodmap -e "keycode 135 = Super_L"
+
+xset s off
+xset -dpms
+
+xhost +SI:localuser:browser
+xhost +SI:localuser:browsertor
