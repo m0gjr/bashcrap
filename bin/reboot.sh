4d093915eb92d66b9c7f138a5d72056dbbca9e7c
m0gjr <m0gjr@github.com>
2020-11-25 15:19:39 +0000

    sudoscripts

--- bin/reboot.sh
+++ bin/reboot.sh
@@ -4,7 +4,7 @@ for server in $(cat ~/serverlist) $(hostname)
 do
 	ssh $server bash <<'EOF' | sed -e "s/^/$server $(date): /"
 		cat /var/run/reboot-required 2> /dev/null &&
-		init 6
+		sudo init 6
 EOF
 
 done
