efc021c7d6962f9f660314526becae64fb55f3c0
m0gjr <54117655+m0gjr@users.noreply.github.com>
2020-12-16 16:03:28 +0000

    Update aws-mfa.sh

--- bin/aws-mfa.sh
+++ bin/aws-mfa.sh
@@ -1,32 +1,47 @@
-#!/bin/bash
+#!/bin/bash -e
 
 AWS_ACCESS_KEY_ID=""
 AWS_SECRET_ACCESS_KEY=""
 AWS_SESSION_TOKEN=""
 
-static_profile="static"
-login_profile="mgmt"
-
-serial=$(aws --profile $static_profile iam list-mfa-devices --query MFADevices[0].SerialNumber | cut -d'"' -f2)
-
-token="$(aws --profile $static_profile sts get-session-token --serial-number $serial --token-code $1)"
-
-aws --profile $login_profile configure set aws_access_key_id $(echo "$token" | grep AccessKeyId | cut -d'"' -f4)
-aws --profile $login_profile configure set aws_secret_access_key $(echo "$token" | grep SecretAccessKey | cut -d'"' -f4)
-aws --profile $login_profile configure set aws_session_token $(echo "$token" | grep SessionToken | cut -d'"' -f4)
-
-for account in $(grep '^\[profile' $HOME/.aws/config | cut -d']' -f1 | cut -d' ' -f2)
+#find all login profiles that need to use MFA to login
+for profile in $(grep '^\[profile' $HOME/.aws/config | cut -d']' -f1 | cut -d' ' -f2)
 do
-        id=$(aws --profile $account configure get accountid)
-        role=$(aws --profile $account configure get role)
+        static_profile="$(aws --profile $profile configure get static_profile)" || continue
 
-        [ -z $id ] && continue
+        aws --profile $profile sts get-caller-identity &> /dev/null && echo "skipping $profile profile" && continue
+
+        serial=$(aws --profile $static_profile iam list-mfa-devices --query MFADevices[0].SerialNumber | cut -d'"' -f2)
+
+        read -p "Enter MFA code for $serial ($profile profile) " code
+
+        [ -z $code ] && continue
+
+        token="$(aws --profile $static_profile sts get-session-token --serial-number $serial --token-code $code)"
+
+        aws --profile $profile configure set aws_access_key_id $(echo "$token" | grep AccessKeyId | cut -d'"' -f4)
+        aws --profile $profile configure set aws_secret_access_key $(echo "$token" | grep SecretAccessKey | cut -d'"' -f4)
+        aws --profile $profile configure set aws_session_token $(echo "$token" | grep SessionToken | cut -d'"' -f4)
+
+        echo "logged in to $profile profile"
+done
+
+#find all role profiles that can be switched to from a login profile
+for profile in $(grep '^\[profile' $HOME/.aws/config | cut -d']' -f1 | cut -d' ' -f2)
+do
+        login_profile="$(aws --profile $profile configure get login_profile)" || continue
+        id="$(aws --profile $profile configure get accountid)" || continue
+        role="$(aws --profile $profile configure get role)" || continue
+
+        ! aws --profile $login_profile sts get-caller-identity &> /dev/null && echo "not logged in to $login_profile profile, skipping $profile profile" && continue
 
         arn="arn:aws:iam::$id:role/$role"
 
-        token="$(aws --profile $login_profile sts assume-role --role-arn $arn --role-session-name $account)"
+        token="$(aws --profile $login_profile sts assume-role --role-arn $arn --role-session-name $profile)"
 
-        aws --profile $account configure set aws_access_key_id $(echo "$token" | grep AccessKeyId | cut -d'"' -f4)
-        aws --profile $account configure set aws_secret_access_key $(echo "$token" | grep SecretAccessKey | cut -d'"' -f4)
-        aws --profile $account configure set aws_session_token $(echo "$token" | grep SessionToken | cut -d'"' -f4)
+        aws --profile $profile configure set aws_access_key_id $(echo "$token" | grep AccessKeyId | cut -d'"' -f4)
+        aws --profile $profile configure set aws_secret_access_key $(echo "$token" | grep SecretAccessKey | cut -d'"' -f4)
+        aws --profile $profile configure set aws_session_token $(echo "$token" | grep SessionToken | cut -d'"' -f4)
+
+        echo "logged in to $profile profile"
 done
