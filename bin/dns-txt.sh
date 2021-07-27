2ddb2bb285bcc954cf044cb8440dc2fbf14dcebb
m0gjr <m0gjr@example.com>
2021-07-27 18:56:32 +0100

    dns-txt

--- bin/dns-txt.sh
+++ bin/dns-txt.sh
@@ -2,7 +2,7 @@
 
 curl "https://www.duckdns.org/update?domains=$CERTBOT_DOMAIN&token=$(cat /etc/local/dns-token)&txt=$CERTBOT_VALIDATION"
 
-until [ "$(host -t txt _acme-challenge.m0gjr.duckdns.org | cut -d\" -f2)" == "$CERTBOT_VALIDATION" ]
+until [ "$(host -t txt _acme-challenge.$CERTBOT_DOMAIN | cut -d\" -f2)" == "$CERTBOT_VALIDATION" ]
 do
 	sleep 10
 done
