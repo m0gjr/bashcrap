b0ad0a2a4a40b51b6cb0a61a9b8d8eb4c5cbead5
m0gjr <m0gjr@example.com>
2025-01-24 21:25:18 +0000

    urlintitle

--- conf/urlintitle.user.js
+++ conf/urlintitle.user.js
@@ -1,3 +1,13 @@
+// ==UserScript==
+// @name         urlintitle
+// @namespace    http://tampermonkey.net/
+// @version      2025-01-24
+// @description  insert url into browser title
+// @author       m0gjr
+// @match        https://*/*
+// @grant        none
+// ==/UserScript==
+
 function urlintitle() {
 	if(! document.title.includes(document.URL)) {
 		console.log('title changed to "%s"', document.title);
